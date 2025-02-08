59088cb788a30e50401348c04187f4ad58ab0db2
UllahLP <mohebullah@outlook.de>
2025-02-08 17:02:42 +0100

    Add files via upload

---
+++ integrate.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tinyexpr.h"
+
+#define STRLEN 100
+
+//To Do
+
+int main(void) {
+	double x;
+	int err;
+	double l;
+	double u;
+	int steps;
+	char line[STRLEN];
+    char fstr[STRLEN];
+
+	// To Do
+}
